712e37bb126b0e68cf32a95e94a14e9d70d06e0a
jolinhappy <jolinhappy1990@hotmail.com>
2021-11-20 19:03:04 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production"
+  ? '/taiwan-travel/'
+  : '/',
   configureWebpack: {
     plugins: [
       Components({
